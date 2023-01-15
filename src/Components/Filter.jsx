01a2c86415a51433d7ae6b76ba43e24a27e46f50
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2023-01-16 04:45:26 +0530

    Update Filter.jsx

--- src/Components/Filter.jsx
+++ src/Components/Filter.jsx
@@ -7,116 +7,83 @@ const Filter = () => {
       <div>
         <div>
           <h3>Colour</h3>
-          <input
-            type="checkbox"
-            value="Novel"
-            // checked={category.includes("Novel")}
-            // onChange={handleFilterCheckbox}
-          />
+          <input type="checkbox" value="Red" />
           <label>Red</label>
         </div>
 
         <div>
-          <input
-            type="checkbox"
-            value="Motivational"
-            // checked={category.includes("Motivational")}
-            // onChange={handleFilterCheckbox}
-          />
+          <input type="checkbox" value="Blue" />
           <label>Blue</label>
         </div>
         <div>
-          <input
-            type="checkbox"
-            value="Science_Fiction"
-            // checked={category.includes("Science_Fiction")}
-            // onChange={handleFilterCheckbox}
-          />
+          <input type="checkbox" value="Green" />
           <label>Green</label>
         </div>
       </div>
 
-      {/* ***************************************************************************** */}
       <div>
         <div>
           <h3>Gender</h3>
-          <input
-            type="checkbox"
-            value="Novel"
-            // checked={category.includes("Novel")}
-            // onChange={handleFilterCheckbox}
-          />
-          <label>Men</label>
-        </div>
-        <div>
-          <input
-            type="checkbox"
-            value="Motivational"
-            // checked={category.includes("Motivational")}
-            // onChange={handleFilterCheckbox}
-          />
-          <label>Women</label>
+
+          <label for="101">
+            <input type="radio" value="Men" name="gender" id="101" />
+            Men
+          </label>
+
+          <label for="102">
+            <input type="radio" value="Women" name="gender" id="102" />
+            Women
+          </label>
         </div>
       </div>
-      {/* ***************************************************************************** */}
       <div>
         <div>
           <h3>Price</h3>
-          <input
-            type="checkbox"
-            value="Novel"
-            // checked={category.includes("Novel")}
-            // onChange={handleFilterCheckbox}
-          />
-          <label>Rs0-Rs250</label>
-        </div>
-        <div>
-          <input
-            type="checkbox"
-            value="Motivational"
-            // checked={category.includes("Motivational")}
-            // onChange={handleFilterCheckbox}
-          />
-          <label>Rs251-Rs450</label>
-        </div>
-        <div>
-          <input
-            type="checkbox"
-            value="Science_Fiction"
-            // checked={category.includes("Science_Fiction")}
-            // onChange={handleFilterCheckbox}
-          />
-          <label>Rs450+</label>
+
+          <div>
+            <label for="201">
+              <input
+                type="radio"
+                value="Rs0-Rs250"
+                name="price-range"
+                id="201"
+              />
+              Rs0-Rs250
+            </label>
+          </div>
+
+          <div>
+            <label for="202">
+              <input
+                type="radio"
+                value="Rs251-Rs450"
+                name="price-range"
+                id="202"
+              />
+              Rs251-Rs450
+            </label>
+          </div>
+
+          <div>
+            <label for="203">
+              <input type="radio" value="Rs450+" name="price-range" id="203" />
+              Rs450+
+            </label>
+          </div>
         </div>
       </div>
-      {/* ***************************************************************************** */}
       <div>
         <div>
           <h3>Type</h3>
-          <input
-            type="checkbox"
-            value="Novel"
-            // checked={category.includes("Novel")}
-            // onChange={handleFilterCheckbox}
-          />
+          <input type="checkbox" value="Polo" />
           <label>Polo</label>
         </div>
         <div>
-          <input
-            type="checkbox"
-            value="Motivational"
-            // checked={category.includes("Motivational")}
-            // onChange={handleFilterCheckbox}
-          />
+          <input type="checkbox" value="Hoodie" />
           <label>Hoodie</label>
         </div>
         <div>
-          <input
-            type="checkbox"
-            value="Science_Fiction"
-            // checked={category.includes("Science_Fiction")}
-            // onChange={handleFilterCheckbox}
-          />
+          <input type="checkbox" value="Basic" />
           <label>Basic</label>
         </div>
       </div>
