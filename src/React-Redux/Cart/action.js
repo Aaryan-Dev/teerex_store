f7c261b2d61ffa29cfb13f08da6a232822fb0886
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2023-01-16 02:17:12 +0530

    Update action.js

--- src/React-Redux/Cart/action.js
+++ src/React-Redux/Cart/action.js
@@ -2,30 +2,14 @@ import * as types from "./actionTypes";
 
 const addInCart = (itemToAdd) => (dispatch) => {
   dispatch({ type: types.GET_CART_SUCCSESS, payload: itemToAdd });
-  // console.log(itemToAdd);
-  // dispatch({ type: types.GET_CART_REQUEST });
-  // return fetch(
-  //   "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/catalogue.json"
-  // )
-  //   .then((res) => res.json())
-  //   .then((res) => {
-  //     dispatch({ type: types.GET_CART_SUCCSESS, payload: res });
-  //   })
-  //   .catch((err) => {
-  //     dispatch({ type: types.GET_CART_FAILURE, payload: err });
-  //   });
-  // axios
-  // .get("http://localhost:8080/books", getBookParams)
-  // .then((res) => {
-  //   dispatch({ type: types.GET_BOOKS_SUCCSESS, payload: res.data });
-  // })
-  // .catch((err) => {
-  //   dispatch({ type: types.GET_BOOKS_FAILURE, payload: err });
-  // });
 };
 
 const updateQuantityCart = (cart) => (dispatch) => {
   dispatch({ type: types.UPDATE_CART_SUCCSESS, payload: cart });
 };
 
-export { addInCart, updateQuantityCart };
+const deletedCartItem = (updatedCart) => (dispatch) => {
+  dispatch({ type: types.DELETE_CART_SUCCSESS, payload: updatedCart });
+};
+
+export { addInCart, updateQuantityCart, deletedCartItem };
