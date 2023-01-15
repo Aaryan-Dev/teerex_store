7431f6eea5c207e18931d054852130dbf69a9563
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2023-01-16 03:48:30 +0530

    Update List.jsx

--- src/Pages/List.jsx
+++ src/Pages/List.jsx
@@ -45,7 +45,12 @@ const List = () => {
     }
   };
 
-  // ******************************************
+  const displayFilter = () => {
+    let append_filter = document.querySelector("#append_filter");
+    let filterCompnent = document.querySelector("#filtered");
+    append_filter.append(filterCompnent);
+    console.log(filterCompnent);
+  };
 
   return (
     <div className={listStyles.products}>
@@ -67,19 +72,33 @@ const List = () => {
             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
           </svg>
         </button>
+        <button
+          onClick={() => displayFilter()}
+          id={listStyles.filterButton}
+          className={listStyles.searchButton}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-funnel"
+            viewBox="0 0 16 16"
+          >
+            <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2h-11z" />
+          </svg>
+        </button>
       </div>
       <div>
-        <div className={listStyles.filter}>
+        <div id="filtered" className={listStyles.filter}>
           <Filter />
         </div>
+        <div id="append_filter"></div>
         <div className={listStyles.list}>
           <div className={listStyles.displayList}>
             {shirts.map((item) => (
               <div key={item.id}>
                 <div id="alert"></div>
-                <div className={listStyles.unavalilable}>
-                  {item.quantity === 0 ? "Item quantity:- 0" : null}
-                </div>
                 <div className={listStyles.image}>
                   <div className={listStyles.name}>{item.name}</div>
                   <img src={item.imageURL} alt="shirt" />
@@ -99,6 +118,9 @@ const List = () => {
                       Add to cart
                     </button>
                   </div>
+                </div>
+                <div className={listStyles.unavalilable}>
+                  {item.quantity === 0 ? "Item quantity:- 0" : null}
                 </div>
               </div>
             ))}
