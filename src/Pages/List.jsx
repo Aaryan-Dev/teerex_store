d556869c7819e26687c0165bfdbc1f76d84439ab
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2023-01-16 04:45:39 +0530

    Update List.jsx

--- src/Pages/List.jsx
+++ src/Pages/List.jsx
@@ -10,16 +10,12 @@ const List = () => {
   const Cart = useSelector((state) => state.CartReducer);
   const dispatch = useDispatch();
 
-  const { shirts, isLoading, isError } = store;
+  const { shirts } = store;
   const { cart } = Cart;
 
   useEffect(() => {
     dispatch(getShirts);
   }, [dispatch]);
-
-  console.log(shirts, isLoading, isError);
-
-  // ******************************************
 
   const addToCart = (id) => {
     let alreadyInCart = false;
@@ -45,13 +41,6 @@ const List = () => {
     }
   };
 
-  // let count = 0;
-
-  // const displayFilter = () => {
-  //   if (count % 2 === 0) {
-  //   }
-  // };
-
   return (
     <div className={listStyles.products}>
       <div className={listStyles.searchBar}>
@@ -72,22 +61,6 @@ const List = () => {
             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
           </svg>
         </button>
-        {/* <button
-          onClick={() => displayFilter()}
-          id={listStyles.filterButton}
-          className={listStyles.searchButton}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            className="bi bi-funnel"
-            viewBox="0 0 16 16"
-          >
-            <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2h-11z" />
-          </svg>
-        </button> */}
       </div>
       <div id={listStyles.column}>
         <div id="filtered" className={listStyles.filter}>
