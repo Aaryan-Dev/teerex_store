2d7d6bf54099df0b018380799350b8f700aed092
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2023-01-16 04:21:08 +0530

    Update List.jsx

--- src/Pages/List.jsx
+++ src/Pages/List.jsx
@@ -45,12 +45,12 @@ const List = () => {
     }
   };
 
-  const displayFilter = () => {
-    let append_filter = document.querySelector("#append_filter");
-    let filterCompnent = document.querySelector("#filtered");
-    append_filter.append(filterCompnent);
-    console.log(filterCompnent);
-  };
+  // let count = 0;
+
+  // const displayFilter = () => {
+  //   if (count % 2 === 0) {
+  //   }
+  // };
 
   return (
     <div className={listStyles.products}>
@@ -72,7 +72,7 @@ const List = () => {
             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
           </svg>
         </button>
-        <button
+        {/* <button
           onClick={() => displayFilter()}
           id={listStyles.filterButton}
           className={listStyles.searchButton}
@@ -87,13 +87,12 @@ const List = () => {
           >
             <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2h-11z" />
           </svg>
-        </button>
+        </button> */}
       </div>
-      <div>
+      <div id={listStyles.column}>
         <div id="filtered" className={listStyles.filter}>
           <Filter />
         </div>
-        <div id="append_filter"></div>
         <div className={listStyles.list}>
           <div className={listStyles.displayList}>
             {shirts.map((item) => (
